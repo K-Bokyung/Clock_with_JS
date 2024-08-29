6ab4ce905c89e1e14522a453aa2f087e303d8342
Bokyung Kwon <tree445560@gmail.com>
2024-08-29 21:31:52 +0900

    add: 1초당 배터리 1%씩 줄어드는 기능 추가

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-// 시간 표시 함수
+// body 출력 시 바로 onload 되는 함수
 function startTime() {
-  const todayTime = new Date();
   // 날짜
+  const todayTime = new Date();
   let y = todayTime.getFullYear();
   let mon = todayTime.getMonth() + 1;
   let d = todayTime.getDate();
@@ -19,13 +19,33 @@ function startTime() {
   let time = h + ':' + m + ':' + s;
   document.getElementById('today_time').innerHTML = time;
 
+  betteryCount();
+
   setTimeout(startTime, 1000);
 }
 
+// 10보다 작은 숫자 앞에 '0'을 붙임
 function checkTime(i) {
-  // 10보다 작은 숫자 앞에 '0'을 붙임
   if (i < 10) {
     i = '0' + i;
   }
   return i;
+}
+
+// 배터리
+let bettery = 100;
+let bettery_out;
+
+function betteryCount() {
+  document.getElementById('bettery__percent').innerHTML = '배터리 : ' + bettery + '%';
+  if (bettery <= 0) {
+    stopCount();
+  } else {
+    --bettery;
+  }
+  bettery_out = setTimeout(betteryCount, 100000);
+}
+
+function stopCount() {
+  clearTimeout(bettery_out);
 }
