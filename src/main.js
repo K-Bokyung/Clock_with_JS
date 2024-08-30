ae6cdfd426f453a09d2d1b61e051bd46fc1a3e7c
Bokyung Kwon <tree445560@gmail.com>
2024-08-30 17:25:36 +0900

    edit: 필수 css 적용

--- src/main.js
+++ src/main.js
@@ -37,6 +37,7 @@ function checkTime(i) {
 // 배터리
 let bettery = 100;
 let bettery_out;
+const timeBox = document.getElementById('box');
 
 function betteryCount() {
   document.getElementById('bettery__percent').innerHTML = '배터리 : ' + bettery + '%';
@@ -52,7 +53,12 @@ function stopCount() {
   clearTimeout(bettery_out);
 }
 
-// 알람 현황
+// 시간 출력창 배경을 검은색으로 바꾸는 함수
+function changeColor() {
+  timeBox.style.backgroundColor = 'black';
+}
+
+// 알람 현황 - 디버깅 중
 function alarm() {
   const alarm_list = [0, 0, 0];
   let txt;
@@ -71,7 +77,6 @@ function alarm() {
         console.log('clicked');
       }
       txt = alarm_list.join('');
-      // console.log(txt);
       document.getElementById('span').innerHTML = txt;
     });
   });
